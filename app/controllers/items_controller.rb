46b3a8c77f0b8f4c42e0ebf8f0e5af537505f469
Jon <jon.guymon@gmail.com>
2023-11-12 18:51:19 +0000

    add and delete working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,29 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.permit(:name, :tag_list)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "items#index"
+
+  resources :items
 end
