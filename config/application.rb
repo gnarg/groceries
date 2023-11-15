421d52fad369bc93b76c94b74e83d969f5a996dd
Jon Guymon <jon@newrelic.com>
2023-11-15 10:33:27 -0800

    more fighting with secrets

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Groceries
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_record.sqlite3_production_warning=false
+    config.active_record.sqlite3_production_warning = false
     config.require_master_key = false
     config.read_encrypted_secrets = false
   end
