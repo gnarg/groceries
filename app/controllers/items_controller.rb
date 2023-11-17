48dd2ad81a8e69503c2126c71635e5ff5c846b17
Jon Guymon <jon@newrelic.com>
2023-11-16 16:15:40 -0800

    live search list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,13 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.where(purchased: params[:purchased] || false)
+    session[:purchased] ||= false
+    session[:purchased] = params[:purchased] if params[:purchased]
+
+    @items = Item.where(purchased: session[:purchased])
+    if (params[:search] and not params[:search].empty?)
+      @items = @items.where("LOWER(name) LIKE ?", "%#{Item.sanitize_sql_like(params[:search].downcase)
+      }%")
+    end
     if (params[:tag])
       @items = @items.tagged_with(params[:tag])
     end
