a39282cff6062d326802329d3a5654df7619443c
Jon Guymon <jon@newrelic.com>
2024-09-13 14:26:18 -0700

    sort by most purchases in the past month

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -10,9 +10,19 @@ let getItems = async (purchased: boolean, tag: string | null, search: string | n
   if (search) {
     filter += ` && (tags ~ '${search}' || name ~ '${search}')`;
   }
-  const results = await pb.collection('groceries_items').getFullList({ filter });
-  // TODO compute purchase count
-  return results;
+  const results = await pb.collection('groceries_items').getFullList({ filter, expand: 'purchases', fields: '*,purchases.created_at' });
+  let items = results.map((item) => {
+    if (item.expand) {
+      let recentPurchases = item.expand.purchases.filter((purchase: any) =>
+        Date.parse(purchase.created) > Date.now() - 1000 * 60 * 60 * 24 * 30);
+      item.count = recentPurchases.length;
+    } else {
+      item.count = 0;
+    }
+    return item;
+  });
+  console.log(items);
+  return items.sort((a, b) => a.count > b.count ? -1 : 1);
 };
 
 export { getItems };
